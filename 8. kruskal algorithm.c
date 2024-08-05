34b85c4195825e2f1d25742c71b6da846d000a18
meghaprasad23 <129981273+meghaprasad23@users.noreply.github.com>
2024-08-06 00:54:11 +0530

    Update 8. kruskal algorithm.c

--- 8. kruskal algorithm.c
+++ 8. kruskal algorithm.c
@@ -1,61 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define INF 999
-#define MAX 9
+typedef struct {
+    int u, v, weight;
+} Edge;
 
-int cost[MAX][MAX], parent[MAX];
-int n, mincost = 0;
-
-int find(int i);
-int unionSets(int i, int j);
-
-int main() {
-    printf("Enter number of vertices: ");
-    scanf("%d", &n);
-
-    printf("Enter the cost adjacency matrix:\n");
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++) {
-            scanf("%d", &cost[i][j]);
-            if (cost[i][j] == 0) cost[i][j] = INF;
-        }
-
-    printf("\nEdges of Minimum Cost Spanning Tree:\n");
-    for (int ne = 1; ne < n; ) {
-        int min = INF, a = -1, b = -1;
-
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                if (cost[i][j] < min) {
-                    min = cost[i][j];
-                    a = i;
-                    b = j;
-                }
-
-        int u = find(a);
-        int v = find(b);
-
-        if (unionSets(u, v)) {
-            printf("%d edge (%d,%d) = %d\n", ne++, a, b, min);
-            mincost += min;
-        }
-
-        cost[a][b] = cost[b][a] = INF;
-    }
-
-    printf("\nMinimum cost = %d\n", mincost);
-    return 0;
-}
+int parent[100], rank[100];
 
 int find(int i) {
-    while (parent[i]) i = parent[i];
-    return i;
+    return parent[i] == i ? i : (parent[i] = find(parent[i]));
 }
 
-int unionSets(int i, int j) {
-    if (i != j) {
-        parent[j] = i;
-        return 1;
+void unionSets(int u, int v) {
+    int rootU = find(u), rootV = find(v);
+    if (rootU != rootV) {
+        if (rank[rootU] > rank[rootV]) parent[rootV] = rootU;
+        else parent[rootU] = rootV, rank[rootU] += (rank[rootU] == rank[rootV]);
     }
+}
+
+int compare(const void *a, const void *b) {
+    return ((Edge *)a)->weight - ((Edge *)b)->weight;
+}
+
+void kruskal(Edge edges[], int numEdges, int numVertices) {
+    Edge mst[100];
+    int mstSize = 0, totalCost = 0;
+
+    for (int i = 0; i < numVertices; i++) parent[i] = i, rank[i] = 0;
+
+    qsort(edges, numEdges, sizeof(Edge), compare);
+
+    for (int i = 0; i < numEdges && mstSize < numVertices - 1; i++) {
+        int u = edges[i].u, v = edges[i].v;
+        if (find(u) != find(v)) {
+            mst[mstSize++] = edges[i];
+            totalCost += edges[i].weight;
+            unionSets(u, v);
+        }
+    }
+
+    printf("Edges in the MST:\n");
+    for (int i = 0; i < mstSize; i++)
+        printf("%d - %d: %d\n", mst[i].u + 1, mst[i].v + 1, mst[i].weight);
+    printf("Total cost: %d\n", totalCost);
+}
+
+int main() {
+    int numVertices = 5, numEdges = 7;
+    Edge edges[] = {{0, 1, 10}, {0, 4, 5}, {1, 2, 1}, {2, 3, 2}, {3, 4, 3}, {4, 2, 6}, {4, 1, 7}};
+    kruskal(edges, numEdges, numVertices);
     return 0;
 }
